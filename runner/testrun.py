12f2f12be781548f91b1243645e3eee06c4fb056
Axel Bock <mr.axel.bock@gmail.com>
2016-01-25 17:14:07 +0100

    implemented run logic for runner.Testrun

--- runner/testrun.py
+++ runner/testrun.py
@@ -6,7 +6,11 @@ import os.path
 import threading
 from re import sub as resub
 import shutil
+import subprocess as sp
 
+
+default_commandline_start = ["docker-compose", "run"]
+default_commandline_end = []
 
 default_config = {
     'environment': {},
@@ -19,7 +23,7 @@ default_config = {
         'run_timeout': 0,
         'test_service': 'test-service',
         # optional values
-        'docker_compose_params': None,
+        'docker_compose_params': [],
         'test_commands': None,
         'test_report_files': None,
     },
@@ -33,22 +37,40 @@ class Testrun(threading.Thread):
 
     def __init__(self, test_definition, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.test_definition = deep_merge(default_config, test_definition)
+        test_definition = deep_merge(default_config, test_definition)
         # quick shortcuts
-        self.test_env = self.test_definition['environment']
-        self.test_meta = self.test_definition['meta']
+        self.test_env = test_definition['environment']
+        self.test_meta = test_definition['meta']
         # start initializing variables - directories and file names
         tmpl = self.test_meta['docker_compose_template']
         self.template = os.path.realpath(tmpl)
-        self.base_dir = os.path.dirname(self.template)
-        self.test_dir = os.path.join(self.base_dir,
-                                     resub("[^a-zA-Z0-9_]", "-", self.name))
+        if self.test_meta.get('test_basedir'):
+            self.base_dir = os.path.realpath(self.test_meta['test_basedir'])
+        else:
+            self.base_dir = os.path.dirname(self.template)
+        self.sanitized_name = resub("[^a-zA-Z0-9_]", "-", self.name)
+        self.test_dir = os.path.join(self.base_dir, self.sanitized_name)
         self.template_name = os.path.basename(tmpl)
-        # auto-add some variables to environment
+        # extend SELF.TEST_ENV with TEST_DIR
         self.test_env['test_dir'] = self.test_dir
-        # result evaluation
+        #### create SELF.COMMANDLINE
+        self.commandline = default_commandline_start.copy()
+        for param in self.test_meta['docker_compose_params']:
+            self.commandline.append(param)
+        for key, val in self.test_env.items():
+            self.commandline.append("-e")
+            self.commandline.append("%s=%s" % (key, val))
+        self.commandline.append("--rm")
+        self.commandline.extend(default_commandline_end.copy())
+        self.commandline.append(self.test_meta['test_service'])
+        # sanitize SELF.TEST_META['test_commands']
+        if type(self.test_meta['test_commands']) == str:
+            self.test_meta['test_commands'] = [self.test_meta['test_commands']]
+        # create SELF.SUCCESS, .RESULT, .EXCEPTION, .REASON
         self.success = True
-        self.report = None
+        self.results = []
+        self.exception = None
+        self.reason = None
 
     def __str__(self):
         return "<runner.Test '%s'>" % self.name
@@ -58,18 +80,46 @@ class Testrun(threading.Thread):
 
     def init_environment(self):
         shutil.rmtree(self.test_dir, ignore_errors=True)
-        os.mkdir(self.test_dir)
+        os.makedirs(self.test_dir)
         os.chdir(self.test_dir)
         # TODO - catch & error handling if template cannot be found.
         tpl = open(self.template, "r").read()
         for key, val in self.test_env.items():
             tpl = tpl.replace("%%%%%s%%%%" % key.upper(), val)
         # TODO (maybe) - catch and error handling if new tmpl cannot be written
-        open(os.path.join(self.testdir,
-                          "docker-compose.yml").write("\n".join(tpl)))
+        open(os.path.join(self.test_dir,
+                          "docker-compose.yml"), "w").write(tpl)
+
+    def run_test_command(self, command=[]):
+        self.results.append(sp.run(
+            self.commandline + command,
+            check=True,
+            stdout=sp.PIPE, stderr=sp.STDOUT,
+            universal_newlines=True,
+        ))
+        tmp = self.results[-1]
+        tmp = tmp
 
     def run(self):
-        self.init_environment()
+        try:
+            self.init_environment()
+            if len(self.test_meta['test_commands']) > 0:
+                for cmd in self.test_meta['test_commands']:
+                    self.run_test_command(cmd.split(" "))
+            else:
+                self.run_test_command()
+        except FileNotFoundError as e:
+            self.exception = e
+            self.success = False
+            self.reason = "Exception"
+            # for now we re-raise to get the stacktrace on the console.
+            raise e
+        except sp.CalledProcessError as e:
+            # we don't re-raise here, that's just the exit from the command
+            # loop above
+            self.success = False
+            self.reason = "Failed command"
+        return self.success
 
     def succeeded(self):
         return self.success
