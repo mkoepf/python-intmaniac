c986a3f11dbefc00edabc6d7a6fa2eab84eeee45
Axel Bock <mr.axel.bock@gmail.com>
2016-01-27 13:48:52 +0100

    added test for subglobal propagation

--- tests/testtemplates.py
+++ tests/testtemplates.py
@@ -4,6 +4,8 @@ from intmaniac import prepare_environment, get_test_set_groups
 from intmaniac import get_and_init_configuration
 
 import unittest
+import os
+import os.path
 
 
 class TestTemplateEmpty(unittest.TestCase):
@@ -94,6 +96,42 @@ class TestTemplateKeysTriple(unittest.TestCase):
                     tcount += 1
                 tscount += 1
 
+
+class TestTemplateSubglobals(unittest.TestCase):
+
+    def setUp(self):
+        self.test_data = "testdata/testconfig_sub_globals.yaml"
+        prepare_environment(["-c", self.test_data])
+        config = get_and_init_configuration()
+        self.tsgs = get_test_set_groups(config)
+
+    def test_subglobal_propagation(self):
+        meta_dict = [
+            ('heey', 'oooh'),
+            ('whoops', 'ydoopsy'),
+            ('and', 'quickly'),
+            ('anastasia', 'mr grey'),
+            ('oh', 'just do it already'),
+        ]
+        env_dict = [
+            ('this_is', 'in all'),
+            ('this_should', 'be cool'),
+            ('and_this', 'is there, too'),
+            ('while_this', 'but this'),
+            ('and', 'one for me, too'),
+        ]
+        self.assertEqual(1, len(self.tsgs))
+        self.assertEqual(1, len(self.tsgs[0]))
+        self.assertEqual(1, len(self.tsgs[0][0].tests))
+        tst = self.tsgs[0][0].tests[0]
+        for pair in ((tst.test_env, env_dict), (tst.test_meta, meta_dict)):
+            tested, tester = pair
+            for key, val in tester:
+                self.assertTrue(key in tested,
+                                "key '%s' is not in %s" % (key, tested))
+                self.assertEqual(val, tested[key],
+                                 "value of tested['%s'] does not match '%s'"
+                                 % (key, val))
 
 if __name__ == "__main__":
     unittest.main()
