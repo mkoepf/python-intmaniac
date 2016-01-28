7f4dd72ec123bc1f2abc6c4938d653b1a450674a
Axel Bock <mr.axel.bock@gmail.com>
2016-01-28 10:53:12 +0100

    updated setup.py with more docs

---
+++ README.rst
@@ -0,0 +1,6 @@
+INTManiac
+=========
+
+A docker-compose based integration / user acceptance test / whatever test tool. For more information please `see the GitHub page`_.
+
+.. _`see the GitHub page`: https://github.com/flypenguin/python-intmaniac
--- setup.py
+++ setup.py
@@ -4,17 +4,27 @@ from setuptools import setup, find_packages
 from pip.req import parse_requirements
 from pip.download import PipSession
 
+import io
+
+
 VERSION = "0.4.0"
 
 
 install_reqs = parse_requirements("./requirements.txt", session=PipSession())
 reqs = [str(ir.req) for ir in install_reqs]
 
+long_description = (
+    io.open('README.rst', encoding='utf-8').read() +
+    '\n' +
+    io.open('CHANGES.rst', encoding='utf-8').read()
+)
+
 setup(
     name             = 'intmaniac',
     packages         = find_packages(),
     version          = VERSION,
     description      = 'A generic integration test tool utilizing docker-compose (for now)',
+    long_description = long_description,
     author           = 'Axel Bock',
     author_email     = 'mr.axel.bock@gmail.com',
     url              = 'https://github.com/flypenguin/python-intmaniac',
